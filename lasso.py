2f43c4c78600a2c719e53a27f5aa5dc4a3e3fa7f
Hanlin Wang <hlwang@Hanlins-MacBook-Air.local>
2023-04-18 23:16:05 -0500

    speedup

--- lasso.py
+++ lasso.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 
 class Lasso:
@@ -36,24 +37,30 @@ class Lasso:
         """
         self.n = None  # sample size
         self.p = None  # feature size
+        self.l = None  # attempts number with different lambda's
         self.y = None  # target, n*1
         self.X = None  # feature, n*p
-        self.features = None  # feature names
-        self.beta = None  # array of beta_j
-        self.beta_0 = None  # beta_0
+        self.features = None  # feature names, 1*j
+        self.beta = None  # array of beta_j, 1*j
+        self.beta_0 = None  # beta_0, scalar
         self.lmd = None  # lambda
         self.max_iter = max_iter  # maximum iterations
         self.stop_crit = stop_crit  # stopping criterion
-        self.y_bar = None  # sample mean of y
-        self.X_bar = None  # sample means of X_j
-        self.y_X = None  # inner products of y and X_j
-        self.o_X = None  # inner products of 1 and X_j
-        self.X_X = None  # inner products of X_k and X_j
+        self.shifts = None  # centralization changes,
+        self.scale = None  # scaling change, scalar
+        self.y_bar = None  # sample mean of y, scalar
+        self.X_bar = None  # sample means of X_j, 1*j
+        self.y_X = None  # inner products of y and X_j, 1*j
+        self.X_X = None  # inner products of X_k and X_j, j*j
         self.lmd_min = None  # minimum lambda
         self.lmd_max = None  # maximum lambda
+        self.step_size = None  # step size of lambda change
         self.lmd_path = None  # path of lambda, 1*l
         self.beta_path = None  # pathes of beta, p*l
-        self.beta_0_path = None  # pathes of beta_0, 1*l
+        self.beta_0_path = None  # path of beta_0, 1*l
+        self.resid_path = None  # pathes of in-sample residual of samples, n*l
+        self.se_path = None  # pathes of in-sample squared error of samples, n*l
+        self.mse_path = None  # path of in-sample mean squared error, 1*l
 
     @staticmethod
     def protected_div(
@@ -78,16 +85,16 @@ class Lasso:
 
     @staticmethod
     def soft_thresh(
-        b: float | np.ndarray, gamma: float | np.ndarray
+        b: int | float | np.ndarray, gamma: int | float | np.ndarray
     ) -> float | np.ndarray:
         """Soft-threshold function, S(b, gamma) = sign(b) * max(0, |n| - gamma), which
-            is the minimizer of (1/2) * (x-b)^2 + gamma * |x|
+        is the minimizer of (1/2) * (x-b)^2 + gamma * |x|
 
         Parameters
         ----------
-        b : float | np.ndarray
+        b : int | float | np.ndarray
             `b` in S(b, gamma)
-        gamma : float | np.ndarray
+        gamma : int | float | np.ndarray
             `gamma` in S(b, gamma)
 
         Returns
@@ -95,30 +102,41 @@ class Lasso:
         float | np.ndarray
             S(b, gamma)
         """
-        if type(b) is not type(gamma):
-            raise Exception("type of `b` should be the same as type of `gamma`")
-        base = np.zeros((len(b),)) if isinstance(b, np.ndarray) else 0.0
+        if isinstance(gamma, np.ndarray) and not isinstance(b, np.ndarray):
+            b = np.full(gamma.shape, b)
+        elif isinstance(b, np.ndarray) and not isinstance(gamma, np.ndarray):
+            gamma = np.full(b.shape, gamma)
+        base = np.zeros(b.shape) if isinstance(b, np.ndarray) else 0
         return np.sign(b) * np.maximum(base, np.abs(b) - gamma)
 
-    def _sample_means(self) -> tuple[np.ndarray]:
-        """Calculate sample means `self.y_bar` and `self.X_bar`
+    def _build(self) -> None:
+        """Build the model
 
-        Returns
-        -------
-        tuple[np.ndarray]
-            `self.y_bar` and `self.X_bar`
+        Normalization (to speed up the convergence of `beta` and `beta_0`)
+            1) centralize: minus each `X_j` by different numbers, `self.shifts`
+            2) scale: divide all `X_j` and `y` by the same number, `self.scale`
+
+        Precomputation (to speed the iteration process in coordinate descent)
+            1) sample means: `self.y_bar` and `self.X_bar`
+            2) inner products: `self.y_X`, `self.o_X` (all 0), and `self.X_X`
+
+        Initialization of `beta_0` and `beta`
+            1) `beta_0` = `y_bar` is already the final solution given `X_j` = 0
+            2) initialize each `beta_j` in `beta` randomly
         """
-        return self.y.mean(), self.X.mean(axis=0)
-
-    def _inner_prods(self) -> tuple[np.ndarray]:
-        """Calculate inner products `self.y_X`, `self.o_X`, and `self.X_X`
-
-        Returns
-        -------
-        tuple[np.ndarray]
-            `self.y_X`, `self.o_X`, and `self.X_X`
-        """
-        return self.y.dot(self.X), np.ones(self.n).dot(self.X), self.X.T @ self.X
+        # normalize
+        self.shifts = self.X.mean(axis=0)
+        self.X -= self.shifts.reshape((1, self.p))
+        self.scale = self.X.std(axis=0).mean()
+        self.X /= self.scale
+        self.y /= self.scale
+        self.lmd /= self.scale**2
+        # precompute
+        self.y_bar, self.X_bar = self.y.mean(), np.zeros(self.p)
+        self.y_X, self.X_X = self.y.dot(self.X), self.X.T @ self.X
+        # initialize beta_0 and beta
+        self.beta_0 = self.y_bar  # final solution
+        self.beta = np.random.randn(self.p)  # randomly initialized
 
     def _inner_prod_z_x(self, j: int) -> float:
         """Calculate `<Z_j, X_j>` as the inner product of `Z_j` and `X_j`
@@ -133,57 +151,44 @@ class Lasso:
         float
             Inner product of `Z_j` and `X_j`
         """
-        res = self.y_X[j] - self.beta_0 * self.o_X[j]
+        res = self.y_X[j] + self.beta[j] * self.X_X[j, j]
         res -= (self.beta.reshape((self.p, 1)) * self.X.T).dot(self.X[:, j]).sum()
-        res += self.beta[j] * self.X_X[j, j]
         return res
 
-    def _initalize_beta(self) -> None:
-        """Initialize `self.beta_0` and `self.beta`"""
-        self.beta_0 = np.random.randn()
-        self.beta = np.random.randn(self.p)
-
-    def _update_beta_j(self, j: int) -> None:
-        """Update `beta_j` in `self.beta`
-
+    def _update_beta(self) -> None:
+        """Update `self.beta`
+        
         minimize (1/2)*(beta_j - \hat{beta_j})^2 + gamma * |beta_j|
         => beta_j^star = S(\hat{beta_j}, gamma)
         where
             \hat{beta_j} = <Z_j, X_j> / ||X_j||^2
             Z_j = (Z_1j, ..., Z_nj)^T
             gamma = n * lamda / ||X_j||^2
-
-        Parameters
-        ----------
-        j : int
-            Feature index
         """
-        Z_j_X_j = self._inner_prod_z_x(j)
-        X_j_X_j = self.X_X[j, j]
-        beta_j_hat = Z_j_X_j / X_j_X_j
-        gamma = self.n * self.lmd / X_j_X_j
-        self.beta[j] = Lasso.soft_thresh(beta_j_hat, gamma)
-
-    def _update_beta(self) -> None:
-        """Update `self.beta`"""
         for j in range(self.p):
-            self._update_beta_j(j)
-
-    def _update_beta_0(self) -> None:
-        """Update `self.beta_0`"""
-        self.beta_0 = self.y_bar - self.beta.dot(self.X_bar)
+            Z_j_X_j = self._inner_prod_z_x(j)
+            X_j_X_j = self.X_X[j, j]
+            beta_j_hat = Z_j_X_j / X_j_X_j
+            gamma = self.n * self.lmd / X_j_X_j
+            self.beta[j] = Lasso.soft_thresh(beta_j_hat, gamma)
 
     def _coordinate_descent(self) -> None:
         """Coordinate descent"""
         for _ in range(self.max_iter):
             all_beta_old = np.insert(self.beta, 0, self.beta_0)
             self._update_beta()
-            self._update_beta_0()
             all_beta_new = np.insert(self.beta, 0, self.beta_0)
             chg = all_beta_new - all_beta_old
             abs_pct_chg = np.abs(Lasso.protected_div(chg, all_beta_old))
             if (abs_pct_chg < self.stop_crit).all():
                 break
+
+    def _adjust(self) -> None:
+        """Adjust `self.beta_0` and `self.lmd` affected by normalization; keep `self.y`
+        and `self.X` the same, which is more convenient for `refit()`
+        """
+        self.beta_0 = self.beta_0 * self.scale - self.shifts.dot(self.beta)
+        self.lmd *= self.scale**2
 
     def fit(
         self,
@@ -191,7 +196,7 @@ class Lasso:
         X: np.ndarray | pd.DataFrame,
         lmd: int | float,
     ) -> "Lasso":
-        """Fit model
+        """Fit the model
 
         Parameters
         ----------
@@ -212,126 +217,17 @@ class Lasso:
         if y.shape[0] != X.shape[0]:
             raise Exception("length of `y` and `X` should be the same")
         # assign attributes
-        self.y, self.X = np.array(y), np.array(X)
         self.features = X.columns if isinstance(X, pd.DataFrame) else None
+        self.y, self.X = np.array(y), np.array(X)
         self.n, self.p = X.shape
         self.lmd = lmd
-        # precompute
-        self.y_bar, self.X_bar = self._sample_means()
-        self.y_X, self.o_X, self.X_X = self._inner_prods()
-        # initialize beta_0 and beta
-        self._initalize_beta()
+        # build
+        self._build()
         # coordinate descent
         self._coordinate_descent()
+        # adjust
+        self._adjust()
         return self
-
-    def refit(
-        self,
-        lmd: int | float,
-    ) -> "Lasso":
-        """Refit with a new `lambda`
-
-        Parameters
-        ----------
-        lmd : int | float
-            `lambda` as the regularization parameter
-
-        Returns
-        -------
-        Lasso
-            The model itself
-        """
-        # set new lambda
-        self.lmd = lmd
-        # initialize beta_0 and beta
-        self._initalize_beta()
-        # coordinate descent
-        self._coordinate_descent()
-        return self
-
-    def path_fit(
-        self,
-        y: np.ndarray | pd.DataFrame,
-        X: np.ndarray | pd.DataFrame,
-        lmd_min: int | float | None = None,
-        lmd_max: int | float | None = None,
-        step_size: int | float = 0.1,
-    ) -> "Lasso":
-        """Fit model with different `lambda`'s to get pathes of `beta`
-
-        Parameters
-        ----------
-        y : np.ndarray | pd.DataFrame
-            Targets
-        X : np.ndarray | pd.DataFrame
-            Features
-        lmd_min : int | float | None, optional
-            Minimum `lambda`, by default None
-        lmd_max : int | float | None, optional
-            Maximum `lambda`, by default None
-        step_size : int | float, optional
-            Step size of `lambda` change, by default 0.1
-
-        Returns
-        -------
-        Lasso
-            The model itself
-        """
-        if lmd_min is None:
-            lmd_min = 0
-        if lmd_max is None:
-            lmd_max = float("inf")
-        if lmd_min < 0:
-            raise Exception("`lmd` should not be less than 0")
-        if lmd_max < lmd_min:
-            raise Exception("`lmd_max` should not be less than `lmd_min`")
-        # assign attributes
-        self.y, self.X = np.array(y), np.array(X)
-        self.features = X.columns if isinstance(X, pd.DataFrame) else None
-        self.n, self.p = X.shape
-        # precompute
-        self.y_bar, self.X_bar = self._sample_means()
-        self.y_X, self.o_X, self.X_X = self._inner_prods()
-        # iterate lambda
-        lmd_path, beta_path, beta_0_path = [], [], []
-        lmd = lmd_min
-        flag = True
-        while flag and lmd <= lmd_max:
-            self.refit(lmd)
-            lmd_path.append(lmd)
-            beta_path.append(self.beta)
-            beta_0_path.append(self.beta_0)
-            lmd += step_size
-            flag = (np.abs(beta_path[-1]) > 1e-9).any()
-        # store results
-        self.lmd_min, self.lmd_max = lmd_min, lmd
-        self.lmd_path = np.array(lmd_path)
-        self.beta_path = np.array(beta_path).T
-        self.beta_0_path = np.array(beta_0_path)
-        return self
-
-    def draw_beta_path(self, legend: bool = False) -> None:
-        """Draw pathes of `beta` with respect to different `lambda`'s
-
-        Parameters
-        ----------
-        legend : bool, optional
-            Plot the legend or not, by default False
-        """
-        if self.beta_path is None:
-            raise Exception("`path_fit()` has not been called")
-        for j in range(self.p):
-            beta_j_path = self.beta_path[j]
-            plt.plot(self.lmd_path, beta_j_path)
-        plt.xlabel("lambda")
-        plt.ylabel("beta")
-        if legend:
-            if self.features is None:
-                legends = list(range(1, 1 + self.p))
-            else:
-                legends = self.features
-            plt.legend(legends, fontsize=8)
-        plt.show()
 
     def predict(self, X: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
         """Predict targets
@@ -353,15 +249,174 @@ class Lasso:
         y_pred = self.beta_0 + X.dot(self.beta)
         return y_pred
 
+    def score(
+        self,
+        y: np.ndarray | pd.DataFrame,
+        X: np.ndarray | pd.DataFrame,
+    ) -> float:
+        if self.beta is None:
+            raise Exception("model has not been fitted")
+        return mean_squared_error(y, self.predict(X))
+
+    def refit(
+        self,
+        lmd: int | float,
+    ) -> "Lasso":
+        """Refit the model with a new `lambda` but the original `y` and `X`
+
+        Parameters
+        ----------
+        lmd : int | float
+            `lambda` as the regularization parameter
+
+        Returns
+        -------
+        Lasso
+            The model itself
+        """
+        # reset lambda
+        self.lmd = lmd / self.scale**2
+        # reset beta and beta_0
+        self.beta_0 = self.y_bar
+        self.beta = np.random.randn(self.p)
+        # coordinate descent
+        self._coordinate_descent()
+        # adjust
+        self._adjust()
+        return self
+
+    def path_fit(
+        self,
+        y: np.ndarray | pd.DataFrame,
+        X: np.ndarray | pd.DataFrame,
+        lmd_min: int | float | None = None,
+        lmd_max: int | float | None = None,
+        step_size: int | float = 0.1,
+    ) -> "Lasso":
+        """Fit the model with different `lambda`'s to get pathes of `beta`
+
+        Parameters
+        ----------
+        y : np.ndarray | pd.DataFrame
+            Targets
+        X : np.ndarray | pd.DataFrame
+            Features
+        lmd_min : int | float | None, optional
+            Minimum `lambda` (including), by default None
+        lmd_max : int | float | None, optional
+            Maximum `lambda` (not including), by default None
+        step_size : int | float, optional
+            Step size of `lambda` change, by default 0.1
+
+        Returns
+        -------
+        Lasso
+            The model itself
+        """
+        if lmd_min is None:
+            lmd_min = 0
+        can_stop = True
+        if lmd_max is None:
+            can_stop = False
+            lmd_max = float("inf")
+        if lmd_min < 0:
+            raise Exception("`lmd` should not be less than 0")
+        if lmd_max < lmd_min:
+            raise Exception("`lmd_max` should not be less than `lmd_min`")
+        # assign attributes
+        self.features = X.columns if isinstance(X, pd.DataFrame) else None
+        self.y, self.X = np.array(y), np.array(X)
+        self.n, self.p = X.shape
+        self.step_size = step_size
+        self.lmd = lmd = lmd_min
+        # build
+        self._build()
+        # iterate lambda
+        lmd_path, beta_path, beta_0_path, resid_path = [], [], [], []
+        flag = True
+        while (flag or can_stop) and lmd < lmd_max:
+            self.refit(lmd)
+            lmd_path.append(lmd)
+            beta_path.append(self.beta)
+            beta_0_path.append(self.beta_0)
+            resid_path.append(y - self.predict(X))
+            lmd += step_size
+            flag = (np.abs(beta_path[-1]) > 1e-9).any()
+        # store results
+        self.l = len(lmd_path)
+        self.lmd_min, self.lmd_max = lmd_min, lmd
+        self.lmd_path = np.array(lmd_path)
+        self.beta_path = np.array(beta_path).T
+        self.beta_0_path = np.array(beta_0_path)
+        self.resid_path = np.array(resid_path).T
+        self.se_path = self.resid_path**2
+        self.mse_path = self.se_path.mean(axis=0)
+        return self
+
+    def draw_beta_path(self) -> None:
+        """Draw the pathes of `beta` with respect to different `lambda`'s"""
+        if self.beta_path is None:
+            raise Exception("`path_fit()` has not been called")
+        # set legends
+        lgds = list(range(1, 1 + self.p)) if self.features is None else self.features
+        # plot
+        plt.figure(figsize=(12, 8))
+        for j in range(self.p):
+            plt.plot(self.lmd_path, self.beta_path[j], label=lgds[j])
+        plt.xlabel("lambda")
+        plt.ylabel("beta")
+        plt.legend(
+            bbox_to_anchor=(0, 1, 1, 0),
+            loc="lower left",
+            mode="expand",
+            ncol=5,
+        )
+        plt.tight_layout()
+        plt.show()
+
+    def draw_mse_path(self) -> None:
+        """Draw the path of in-sample MSE (mean squared error) as dark curves and the
+        pathes of in-sample SE (squared error) of each sample as light curves
+        """
+        if self.mse_path is None:
+            raise Exception("`path_fit()` has not been called")
+        # plot
+        plt.figure(figsize=(12, 8))
+        for i in range(self.n):
+            plt.plot(self.lmd_path, self.se_path[i], color="#87CEFA", alpha=0.3)
+        plt.plot(self.lmd_path, self.mse_path, color="b")
+        plt.xlabel("lambda")
+        plt.ylabel("MSE (dark) / SE (light)")
+        plt.show()
+
 
 if __name__ == "__main__":
-    n, p = 1000, 3
-    X = np.random.normal(0, 1, (n, p))
+    import time
+
+    n, p = 100, 20
+    X = np.random.uniform(10, 20, (n, p))
     eps = np.random.randn(n)
-    beta_true = np.array([3, -17, 5])
+    beta_true = np.random.randint(-10, 10, p)
     beta_0_true = 0
     y = beta_0_true + X.dot(beta_true) + eps
 
+    # from sklearn.linear_model import Lasso as L
+    # m = L(alpha=1)
+    # start = time.time()
+    # m.fit(X, y)
+    # print(m.coef_)
+    # print(time.time() - start, "t")
+
+    # lasso = Lasso()
+    # start = time.time()
+    # lasso.fit(y, X, 2)
+    # lasso.refit(1)
+    # print(lasso.beta)
+    # print(time.time() - start, "t")
+
     lasso = Lasso()
-    lasso.path_fit(y, X)
-    lasso.draw_beta_path(True)
+    start = time.time()
+    lasso.path_fit(y, X, step_size=0.2)
+    print(time.time() - start)
+    lasso.draw_beta_path()
+    # lasso.draw_mse_path()
